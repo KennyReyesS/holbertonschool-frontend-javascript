dc0f9b2bfa1b778c1d5d1ac489ea4f64bb463513
KennyReyesS <kennyreyes2000@gmail.com>
2021-11-29 19:08:20 -0500

    one line long

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const char = Array.from(string)
-  return array1.concat(array2, char)
+  return array1.concat(array2, Array.from(string))
 }
