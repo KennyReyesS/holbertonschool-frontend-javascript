b1ed52659f64a5012d6293cf236f91b010edb1b7
KennyReyesS <kennyreyes2000@gmail.com>
2021-12-01 13:11:46 -0500

    return added

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((res) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((res) => {
     console.log(
       res[0].body,
       res[1].firstName,
       res[1].lastName,
     );
   })
-    .catch(() => { console.log('Signup system offline'); });
+    .catch(() => console.log('Signup system offline'));
 }
